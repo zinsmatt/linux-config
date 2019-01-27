adfdfff9d5e1f8ecc1e67be2aa6a769983693e4c
zinsmatt <zins.matthieu@gmail.com>
2019-01-27 16:41:12 +0100

    Add config for terminator

--- terminator.sh
+++ terminator.sh
@@ -1,4 +1,4 @@
 sudo apt -y install terminator
 
 # copy config file
-cp terminator-config /home/matthieu/.config/terminator/config
+cp configs/terminator-config /home/matthieu/.config/terminator/config

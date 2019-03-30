88cfed0d5bbb76bef323381e79f4417c3f92f018
Matthieu Zins <zins.matthieu@gmail.com>
2019-03-31 00:24:28 +0100

    update cmake.sh

--- cmake.sh
+++ cmake.sh
@@ -6,7 +6,7 @@ cd ~/lib
 
 git clone https://github.com/Kitware/CMake.git
 cd CMake
-git checkout v3.10.3
+git checkout v3.10.2
 
 
 me=$(whoami)
@@ -15,20 +15,20 @@ make -j8
 make install
 
 echo -e "\n# CMake" >> ~/.bashrc
-echo -e 'export PATH=$PATH:/home/matthieu/opt/cmake/bin\n' >> ~/.bashrc
+echo -e 'export PATH=$PATH:/home/$me/opt/cmake/bin\n' >> ~/.bashrc
 source  ~/.bashrc
 
 # ccmake
-sudo apt -y install libncurses5-dev
+sudo apt -y install libncurses5-dev zlib1g-dev libcurl4-openssl-dev
 cmake . -DBUILD_CursesDialog=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_USE_SYSTEM_CURL=ON
 make -j8
 make install
 
 # cmake-gui
 # Requires Qt5 installed
-cmake . -DBUILD_QtDialog=ON -DQt5Core_DIR=/home/$me/Qt/5.9.6/gcc_64/lib/cmake/Qt5Core \
-        -DQt5Gui_DIR=/home/$me/Qt/5.9.6/gcc_64/lib/cmake/Qt5Gui \
-        -DQt5Widgets_DIR=/home/$me/Qt/5.9.6/gcc_64/lib/cmake/Qt5Widgets
+cmake . -DBUILD_QtDialog=ON -DQt5Core_DIR=/home/$me/Qt/5.10.1/gcc_64/lib/cmake/Qt5Core \
+        -DQt5Gui_DIR=/home/$me/Qt/5.10.1/gcc_64/lib/cmake/Qt5Gui \
+        -DQt5Widgets_DIR=/home/$me/Qt/5.10.1/gcc_64/lib/cmake/Qt5Widgets
 make -j8
 make install
 

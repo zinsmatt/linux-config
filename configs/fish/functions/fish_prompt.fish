8e952711a506fa717a54d889e425722eef8ba847
zinsmatt <zins.matthieu@gmail.com>
2020-09-01 13:30:47 +0200

    Update fish config to avoid the need of additional file fish_is_root

--- configs/fish/functions/fish_prompt.fish
+++ configs/fish/functions/fish_prompt.fish
@@ -8,21 +8,17 @@ function fish_prompt --description "Write out the prompt"
     set -l color_cwd
     set -l suffix
 
-    if fish_is_root_user
-        if set -q fish_color_cwd_root
-            set color_cwd $fish_color_cwd_root
-        else
-            set color_cwd $fish_color_cwd
-        end
-
-        set suffix '#'
-    else
-        set color_cwd $fish_color_cwd
-        set color_cwd 32a852
-        set suffix '>'
-    end
-    set color_dir $fish_color_param
-    echo -n -s (set_color $color_cwd) "$USER" @ (prompt_hostname) ' ' (set_color $color_dir) (prompt_pwd) (set_color normal) "$suffix "
+  switch $USER
+  case root toor
+    set color_cwd $fish_color_cwd_root
+    set suffix '#'
+  case '*'
+    set color_cwd $fish_color_cwd
+    set color_cwd 32a852
+    set suffix '>'
+  end
+  set color_dir $fish_color_param
+  echo -n -s (set_color $color_cwd) "$USER" @ (prompt_hostname) ' ' (set_color $color_dir) (prompt_pwd) (set_color normal) "$suffix "
     
     
     
